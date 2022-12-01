888333d1b522318fd3bf8817aff541355659d8e8
Dante Mozombite <dantemozombitequiroz@gmail.com>
2022-12-01 16:57:38 -0500

    yes

---
+++ curso-react1/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Clase3 from "./components/Clase3";
+
+const App = () => {
+
+  let saludo = "hola mundo"
+  let name = "Dante"
+  let edad = 20
+
+  return (
+    <div className="container">
+      <Clase3 />
+    </div>
+  )
+
+
+}
+export default App;
+
+
---
+++ curso-react1/src/components/Clase3.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Clase3 = () => {
+
+    let edad = 21
+    let nombre = "Dante"
+
+    console.log( `soy mayor de edad?: ${edad >= 18 ? nombre + ", si lo eres" : "no, no lo soy" }  `);
+
+
+
+    return (
+        <div>Clase 3</div>
+    )
+}
+export default Clase3;
