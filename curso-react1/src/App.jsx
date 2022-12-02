7e487c665bd91cd5f723ae68654e559f1bfbff00
Dante Mozombite <dantemozombitequiroz@gmail.com>
2022-12-02 13:41:34 -0500

    con

--- curso-react1/src/App.jsx
+++ curso-react1/src/App.jsx
@@ -1,29 +1,12 @@
 import React from "react";
 import Navbar from "./components/navbar";
+import Producto from "./components/Producto";
 
 const App = () => {
-  let imagen_producto = "TrbaakvbkufSLgX6S-CaldoSinPresa.jpg"
-  let nombre_producto = "Caldo de Pollo"
-  let description = "Lo mejor de lo mejor! Te lo juro por diegito maradona."
-  
-
-  return (
-
-    
+  return ( 
     <div className="container" >
-      <Navbar/>
-      
-      
-      <div className="row ">
-        <div className="col-md-5 offset-md-2" >
-          <img src={imagen_producto} alt={nombre_producto} className="img-fluid" />
-        </div>
-        <div className="col-md-4">
-          <h1>{nombre_producto}</h1>
-          <h3> Especial de la casa </h3>
-          <p>{description}</p>
-        </div>
-      </div>
+      <Navbar nombre={"Contacto"} />
+      <Producto/>
     </div>
   )
 
--- curso-react1/src/components/navbar.jsx
+++ curso-react1/src/components/navbar.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 
-const Navbar = () => {
+const Navbar = (props) => {
     return (
-        <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">THE SOUP!</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">THE SOUP!</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Casa</a>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Casa</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Platos</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Platos</a>
               </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">{props.nombre}</a>
+              </li>
+
             </ul>
           </div>
         </div>
