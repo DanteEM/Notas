dfe9b752ec6ea2a54fa3ffc762c04f9fa3debe3d
Dante Mozombite <dantemozombitequiroz@gmail.com>
2022-12-02 11:50:40 -0500

    components

--- curso-react1/src/components/navbar.jsx
+++ curso-react1/src/components/navbar.jsx
@@ -2,6 +2,25 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <h1></h1>
+        <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">THE SOUP!</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Casa</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Platos</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     )
 }
+
+export default Navbar
