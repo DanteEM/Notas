c1e642b9a8c6fa442d0c741ddaa5124f304c23a5
Dante Mozombite <dantemozombitequiroz@gmail.com>
2022-12-17 20:59:01 -0500

    da

--- curso-react1/src/components/Item.jsx
+++ curso-react1/src/components/Item.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 
 const Item = ({ item }) => {
     return (
         <div className="col-md-4">
-            <Link to={"/item/1"} className="text-dark text-decoration-none" />
+            <Link to={"/item/" + item.id} className={"text-dark text-decoration-none"}>
                <div className="card text-center border border-0">
                  <img src={item.imagen} class="card-img-top" alt={item.nombre} />
                  <div className="card-body">
                     <p className="card-text">{item.nombre}</p>
                  </div>
                </div>
-            <Link />
+               </Link>
         </div>
     )
 };
--- curso-react1/src/components/ItemDetail.jsx
+++ curso-react1/src/components/ItemDetail.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const ItemDetail = ({item}) => {
     return(
+
         <div className="row">
             <div className="col-md-4 offset-md-2">
                 <img src={item.imagen} alt={item.nombre} className="img-fluid" />
